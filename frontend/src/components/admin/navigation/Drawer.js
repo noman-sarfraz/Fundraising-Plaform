1129c8702f7b97211ee744a799cb03bfccb2be91
Noman Sarfraz <nomansarfrazf19@gmail.com>
2024-11-28 13:15:24 +0500

    testing

--- frontend/src/components/admin/navigation/Drawer.js
+++ frontend/src/components/admin/navigation/Drawer.js
@@ -238,7 +238,7 @@ function DrawerComponent({ open, toggleDrawer }) {
                       // color: "white",
                     },
                   }}
-                >
+                > 
                   {item.icon ? (
                     item.icon
                   ) : (
